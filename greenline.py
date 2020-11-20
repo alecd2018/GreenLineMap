379d3182fad834cf1a09997a2e69f8aa7b8ca52c
alecd2018 <agd57@cornell.edu>
2020-11-20 13:13:02 -0500

    Created looping prediction times

--- greenline.py
+++ greenline.py
@@ -1,23 +1,48 @@
 import requests
 import json
 import datetime
+import time
 
-url = "https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=3&filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-stpul"
-resp = requests.get(url)
+def getPredictionData():
+    url = "https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=3&filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-stpul"
+    resp = requests.get(url)
 
-data = json.loads(resp.text)
+    data = json.loads(resp.text)
 
-trainList = data["data"]
+    return data["data"]
 
-if (len(trainList) == 0):
-    print("No trains")
-elif (len(trainList) == 1):
-    trainArrival0 = trainList[0]["attributes"]["arrival_time"]
-    arrvialTime0 = datetime.datetime.strptime(trainArrival0, '%Y-%m-%dT%H:%M:%S-5:00')
-    arrvialTime0 = arrvialTime0.time()
-else:
-    trainArrival0 = trainList[0]["attributes"]["arrival_time"]
-    arrvialTime0 = datetime.datetime.strptime(trainArrival0, '%Y-%m-%dT%H:%M:%S-5:00')
-    arrvialTime0 = arrvialTime0.time()
+def listPredictTimes(trainList):
+    timeList = []
+    for train in trainList:
+        trainArrival = train["attributes"]["arrival_time"]
+        arrivalTime = datetime.datetime.strptime(trainArrival, '%Y-%m-%dT%H:%M:%S-05:00') 
+        trainDeparture = train["attributes"]["departure_time"]
+        departTime = datetime.datetime.strptime(trainDeparture, '%Y-%m-%dT%H:%M:%S-05:00') 
 
-    print(arrvialTime0)
+        absDiffMinutes = arrivalTime.minute - datetime.datetime.now().minute
+        absDiffSeconds = arrivalTime.second - datetime.datetime.now().second
+        diffMinutes = absDiffMinutes if (absDiffSeconds >= 0) else absDiffMinutes - 1
+        diffSeconds = absDiffSeconds if (absDiffSeconds >= 0) else absDiffSeconds + 60 
+
+        if diffMinutes >= 0:
+            timeList.append("Arriving in "+str(diffMinutes) + "m"+ str(diffSeconds) + "s")
+
+        # if (diffMinutes < 0 or (diffMinutes ==0 and diffSeconds <0)):
+
+        absDiffMinutes = departTime.minute - datetime.datetime.now().minute
+        absDiffSeconds = departTime.second - datetime.datetime.now().second
+        diffMinutes = absDiffMinutes if (absDiffSeconds >= 0) else absDiffMinutes - 1
+        diffSeconds = absDiffSeconds if (absDiffSeconds >= 0) else absDiffSeconds + 60 
+
+        if diffMinutes >= 0:
+            timeList.append("Leaving in "+str(diffMinutes) + "m"+ str(diffSeconds) + "s")
+
+    print(timeList)
+
+def run():
+    while True:
+        data = getPredictionData()
+        listPredictTimes(data)
+        time.sleep(10)
+
+run()
